a4ea21eae1edea499e96264078e3afab6da92e77
niravassar <assarn@objectcomputing.com>
2019-10-21 12:05:13 -0500

    code paths changed to java

---
+++ complete/src/main/java/micronaut/cache/hazelcast/service/HazelcastCacheAdditionalSettings.java
@@ -0,0 +1,18 @@
+package micronaut.cache.hazelcast.service
+
+import io.micronaut.cache.hazelcast.HazelcastClientConfiguration
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
+
+import javax.inject.Singleton
+
+@Singleton
+class HazelcastCacheAdditionalSettings implements BeanCreatedEventListener<HazelcastClientConfiguration> { // <1>
+
+    @Override
+    HazelcastClientConfiguration onCreated(BeanCreatedEvent<HazelcastClientConfiguration> event) {
+        HazelcastClientConfiguration hazelcastClientConfiguration = event.getBean() // <2>
+        hazelcastClientConfiguration.getNetworkConfig().setConnectionTimeout(8888) // <3>
+        hazelcastClientConfiguration
+    }
+}
---
+++ complete/src/main/java/micronaut/cache/hazelcast/service/MessageService.java
@@ -0,0 +1,31 @@
+package micronaut.cache.hazelcast.service
+
+import io.micronaut.cache.annotation.CacheInvalidate
+import io.micronaut.cache.annotation.CachePut
+import io.micronaut.cache.annotation.Cacheable
+
+import javax.inject.Singleton
+
+@Singleton
+class MessageService {
+
+    int invocationCounter = 0
+
+    @Cacheable("my-cache") // <1>
+    String returnMessage(String message) {
+        ++invocationCounter
+        message+"_FromInsideMethodReturnMessage"
+    }
+
+    @CacheInvalidate(value="my-cache", all=true) // <2>
+    String invalidateAndReturnMessage(String message) {
+        ++invocationCounter
+        message+"_FromInsideMethodInvalidateAndReturnMessage"
+    }
+
+    @CachePut("my-cache") // <3>
+    String putReturnMessage(String message) {
+        ++invocationCounter
+        message+"_FromInsideMethodPutAndReturnMessage"
+    }
+}
